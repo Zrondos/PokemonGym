8aad431833afba8dbac1bb42de50f264fcf9c0f7
kevin wong <kevinwong247@gmail.com>
2018-08-25 14:29:42 -0400

    buttons and images

--- main.js
+++ main.js
@@ -11,82 +11,142 @@ let pokemonGym=new Gym("Elite 2")
 function renderGym(){
     pokemonGym.trainers.push(ZachTrainer)
     pokemonGym.trainers.push(KevinTrainer)
+     
     // let hpElement=document.createElement("li");
     //         var hp=document.createTextNode("Hp:"+pokemon.Hp)
     //         hpElement.appendChild(hp);
     // console.log(pokemonGym.trainers[0].all()[0])
     // console.log(pokemonGym.trainers)
-    function displayStats(pokemon){
-        let nameElement=document.createElement("li");
-        var name=document.createTextNode("Name: "+pokemon.name)
-        nameElement.appendChild(name);
-        stats=document.getElementsByClassName("statsList")
-        stats[0].innerHTML = " ";
-        stats[0].appendChild(nameElement)
-        /////Display HP
-        let hpElement=document.createElement("li");
-        var hp=document.createTextNode("Hp: "+pokemon.Hp)
-        hpElement.appendChild(hp);
-        stats=document.getElementsByClassName("statsList")
-        
-        stats[0].appendChild(hpElement)
-        
-        ////////display Attack Stats
-        let AttackElement=document.createElement("li");
-        var Attack=document.createTextNode("Attack: "+pokemon.Attack)
-        AttackElement.appendChild(Attack);
-        stats=document.getElementsByClassName("statsList");
-        stats[0].appendChild(AttackElement)
-        ////display Defense stats
-        let DefenseElement=document.createElement("li");
-        var Defense=document.createTextNode("Defense: "+pokemon.Defense)
-        DefenseElement.appendChild(Defense);
-        stats=document.getElementsByClassName("statsList")
-        stats[0].appendChild(DefenseElement)
-        ////display Special Attack stats
-        let SpecialAttackElement=document.createElement("li");
-        var SpecialAttack=document.createTextNode("SpecialAttack: "+pokemon.SpecialAttack)
-        SpecialAttackElement.appendChild(SpecialAttack);
-        stats=document.getElementsByClassName("statsList")
-        stats[0].appendChild(SpecialAttackElement)
-        ////display Special Defense stats
-        let SpecialDefenseElement=document.createElement("li");
-        var SpecialDefense=document.createTextNode("SpecialDefense: "+pokemon.SpecialDefense)
-        SpecialDefenseElement.appendChild(SpecialDefense);
-        stats=document.getElementsByClassName("statsList")
-        stats[0].appendChild(SpecialDefenseElement)
-        ////display Speed stats
-        let SpeedElement=document.createElement("li");
-        var Speed=document.createTextNode("Speed: "+pokemon.Speed)
-        SpeedElement.appendChild(Speed);
-        stats=document.getElementsByClassName("statsList")
-        stats[0].appendChild(Speed)
-    }
-  
-    // displayStats((pokemonGym.trainers[0].myPokemon[0]))
-    document.getElementById("button1").addEventListener("click", function(){displayStats((pokemonGym.trainers[0].myPokemon[0])),myFunction("ninetails")});
-    document.getElementById("button2").addEventListener("click", function(){displayStats((pokemonGym.trainers[0].myPokemon[1])),myFunction("garchomp")});
-    document.getElementById("button3").addEventListener("click", function(){displayStats((pokemonGym.trainers[0].myPokemon[2])),myFunction("tapu-koko")});
-    document.getElementById("button4").addEventListener("click", function(){displayStats((pokemonGym.trainers[1].myPokemon[0])),myFunction("snorlax")});
-    document.getElementById("button5").addEventListener("click", function(){displayStats((pokemonGym.trainers[1].myPokemon[1])),myFunction("umbreon")});
-    document.getElementById("button6").addEventListener("click", function(){displayStats((pokemonGym.trainers[1].myPokemon[2])),myFunction("gyarados")});
-    function myFunction(idname) {
-        var x = document.getElementById(idname);
+    function displayStats(pokemon, idname, imagesrc){
+            var x = document.getElementById(idname);
             if (x.style.display === "none") {
                 x.style.display = "block";
             } else {
                 x.style.display = "none";
             }
-            
+        let ul = document.createElement("ul")
+        let nameElement=document.createElement("li");
+        var name=document.createTextNode("Name: "+pokemon.name);
+        nameElement.appendChild(name);
+        ul.appendChild(nameElement);
+        stats=document.getElementById(idname);
+        stats.innerHTML = " ";
+        stats.appendChild(nameElement);
+        
+        
+
+        let hpElement=document.createElement("li");
+        var hp=document.createTextNode("HP: "+pokemon.Hp);
+        hpElement.appendChild(hp);
+        ul.appendChild(hpElement);
+        stats=document.getElementById(idname);
+        stats.appendChild(hpElement);
 
         
+        let attackElement=document.createElement("li");
+        var attack=document.createTextNode("Attack: "+pokemon.Attack);
+        attackElement.appendChild(attack);
+        ul.appendChild(attackElement);
+        stats=document.getElementById(idname);
+        stats.appendChild(attackElement);
+
+        let defenseElement=document.createElement("li");
+        var defense=document.createTextNode("Defense: "+pokemon.Defense);
+        defenseElement.appendChild(defense);
+        ul.appendChild(defenseElement);
+        stats=document.getElementById(idname);
+        stats.appendChild(defenseElement);
+
+        let specialattackElement=document.createElement("li");
+        var specailattack=document.createTextNode("SpecialAttack: "+pokemon.SpecialAttack);
+        specialattackElement.appendChild(specailattack);
+        ul.appendChild(specialattackElement);
+        stats=document.getElementById(idname);
+        stats.appendChild(specialattackElement);
+        
+        let specialdefenseElement=document.createElement("li");
+        var specialdefense=document.createTextNode("SpecialDefense: "+pokemon.SpecialDefense);
+        specialdefenseElement.appendChild(specialdefense);
+        ul.appendChild(specialdefenseElement);
+        stats=document.getElementById(idname);
+        stats.appendChild(specialdefenseElement);
+
+        let speedElement=document.createElement("li");
+        var speed=document.createTextNode("Speed: "+pokemon.Speed);
+        speedElement.appendChild(speed);
+        ul.appendChild(speedElement);
+        stats=document.getElementById(idname);
+        stats.appendChild(speedElement);
+
+      
+        ul.setAttribute("class", "statsList col s6");
+        let img = document.createElement('img');
+        img.setAttribute('src',imagesrc);
+        img.setAttribute("class", "col s6");
+        stats.appendChild(img);
+
+
+        /////Display HP
+        // let hpElement=document.createElement("li");
+        // var hp=document.createTextNode("Hp: "+pokemon.Hp)
+        // hpElement.appendChild(hp);
+        // stats=document.getElementsByClassName("statsList")
+        
+        // stats[0].appendChild(hpElement)
+        
+        ////////display Attack Stats
+        // let AttackElement=document.createElement("li");
+        // var Attack=document.createTextNode("Attack: "+pokemon.Attack)
+        // AttackElement.appendChild(Attack);
+        // stats=document.getElementsByClassName("statsList");
+        // stats[0].appendChild(AttackElement)
+        ////display Defense stats
+        // let DefenseElement=document.createElement("li");
+        // var Defense=document.createTextNode("Defense: "+pokemon.Defense)
+        // DefenseElement.appendChild(Defense);
+        // stats=document.getElementsByClassName("statsList")
+        // stats[0].appendChild(DefenseElement)
+        ////display Special Attack stats
+        // let SpecialAttackElement=document.createElement("li");
+        // var SpecialAttack=document.createTextNode("SpecialAttack: "+pokemon.SpecialAttack)
+        // SpecialAttackElement.appendChild(SpecialAttack);
+        // stats=document.getElementsByClassName("statsList")
+        // stats[0].appendChild(SpecialAttackElement)
+        ////display Special Defense stats
+        // let SpecialDefenseElement=document.createElement("li");
+        // var SpecialDefense=document.createTextNode("SpecialDefense: "+pokemon.SpecialDefense)
+        // SpecialDefenseElement.appendChild(SpecialDefense);
+        // stats=document.getElementsByClassName("statsList")
+        // stats[0].appendChild(SpecialDefenseElement)
+        ////display Speed stats
+        // let SpeedElement=document.createElement("li");
+        // var Speed=document.createTextNode("Speed: "+pokemon.Speed)
+        // SpeedElement.appendChild(Speed);
+        // stats=document.getElementsByClassName("statsList")
+        // stats[0].appendChild(Speed)
     }
-    myFunction("ninetails")
-    myFunction("garchomp")
-    myFunction("tapu-koku")
-    myFunction("snorlax")
-    myFunction("umbreon")
-    myFunction("gyarados")
+    // function myFunction(idname) {
+        
+    // }
+    // displayStats((pokemonGym.trainers[0].myPokemon[0]))
+    let ninetailsimg = "https://pa1.narvii.com/6267/f29a8614bbe48096572657ccb056f7002666f0d9_128.gif"
+    let garchompimg = "https://cdn.bulbagarden.net/upload/8/85/PCP445.png"
+    let tapukokoimg = "https://veekun.com/dex/media/pokemon/main-sprites/ultra-sun-ultra-moon/785.png"
+    let snorlaximg = "https://pa1.narvii.com/6464/eeb6c7a0189e04d1444e8c40e598f3d56390bb32_128.gif"
+    let umbreonimg = "https://veekun.com/dex/media/pokemon/conquest/197.png"
+    let gyaradosimg = "https://cdn.bulbagarden.net/upload/9/9d/PCP130.png"
+    document.getElementById("button1").addEventListener("click", function(){displayStats(pokemonGym.trainers[0].myPokemon[0],"ninetails",ninetailsimg)});
+    document.getElementById("button2").addEventListener("click", function(){displayStats(pokemonGym.trainers[0].myPokemon[1],"garchomp",garchompimg)});
+    document.getElementById("button3").addEventListener("click", function(){displayStats(pokemonGym.trainers[0].myPokemon[2],"tapu-koko",tapukokoimg)});
+    document.getElementById("button4").addEventListener("click", function(){displayStats(pokemonGym.trainers[1].myPokemon[0],"snorlax",snorlaximg)});
+    document.getElementById("button5").addEventListener("click", function(){displayStats(pokemonGym.trainers[1].myPokemon[1],"umbreon",umbreonimg)});
+    document.getElementById("button6").addEventListener("click", function(){displayStats(pokemonGym.trainers[1].myPokemon[2],"gyarados",gyaradosimg)});
+    // myFunction("ninetails")
+    // myFunction("garchomp")
+    // myFunction("tapu-koku")
+    // myFunction("snorlax")
+    // myFunction("umbreon")
+    // myFunction("gyarados")
 }
 
 delayAxios(pokemonGym,renderGym)
